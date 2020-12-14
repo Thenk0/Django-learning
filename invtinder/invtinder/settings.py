c9abc636aa720937efbf8836fdc6ad8493a22ffe
Vlad <vladikvd@gmail.com>
2020-12-14 16:27:36 +0600

    added basic styling to allauth

--- invtinder/invtinder/settings.py
+++ invtinder/invtinder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,8 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "tinder.apps.TinderConfig",
-    # AllAuth apps
     "django.contrib.sites",
+    # AllAuth apps
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
@@ -55,12 +56,17 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
 ROOT_URLCONF = "invtinder.urls"
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "tinder/templates"), os.path.join(BASE_DIR, "templates", "allauth")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,6 +132,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+SITE_ID = 1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- invtinder/tinder/views.py
+++ invtinder/tinder/views.py
@@ -2,5 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+posts = [
+    {
+        "title": "1 post",
+        "text": "this is a 1st post text",
+    },
+    {
+        "title": "2 post",
+        "text": "this is a 2nd post text",
+    },
+    {
+        "title": "3 post",
+        "text": "this is a 3rd post text",
+    },
+]
+test = [{"test": 123}]
+
+
 def index(request):
-    return HttpResponse("")
+    context = {"posts": posts, "test": test}
+    return render(request, "tinder/index.html", context)
